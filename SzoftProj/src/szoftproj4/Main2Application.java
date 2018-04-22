4070efc29d12662762ab7e1031497c1f385848c9
ipoly <ipigabi98@gmail.com>
2018-04-22 17:08:07 +0200

    A főmenü megtervezése, a "loadMap"-es dolgokban kellene vhogy beolvasni a pályákat Zsombi

---
+++ SzoftProj/src/szoftproj4/Main2Application.java
@@ -0,0 +1,107 @@
+package szoftproj4;
+
+import java.util.Scanner;
+
+public class Main2Application {
+	
+	public static void Main(String args[]) {
+		
+		Scanner scanner = new Scanner(System.in);	
+		String reg=" ";	
+		String myString=scanner.nextLine();		
+		scanner.close();	
+		String[] cmd=myString.split(reg);
+		
+		if (cmd[0].equals("loadMap")) {
+			if (cmd[1].equals("Test1")) {
+				
+			}
+			
+			if (cmd[1].equals("Test2")) {
+				
+			}
+			
+			if (cmd[1].equals("Test3")) {
+				
+			}
+			
+			if (cmd[1].equals("Test4")) {
+				
+			}
+			
+			if (cmd[1].equals("Test5")) {
+				
+			}
+			
+			if (cmd[1].equals("Test6")) {
+				
+			}
+		}
+		
+		if (cmd[0].equals("moveDolgozo")) {
+			if (cmd[1].equals("UP")) {
+				
+			}
+			
+			if (cmd[1].equals("DOWN")) {
+				
+			}
+			
+			if (cmd[1].equals("LEFT")) {
+				
+			}
+			
+			if (cmd[1].equals("RIGHT")) {
+				
+			}
+		}
+
+		if (cmd[0].equals("listDolgozo")) {
+			
+		}
+		
+		if (cmd[0].equals("listAkadaly")) {
+			
+		}
+		
+		if (cmd[0].equals("listLada")) {
+			
+		}
+		
+		if (cmd[0].equals("listDolgozo")) {
+			
+		}
+		
+		if (cmd[0].equals("listAkadaly")) {
+			
+		}
+		
+		if (cmd[0].equals("listCelmezo")) {
+			
+		}
+		
+		if (cmd[0].equals("listDolgozo")) {
+			
+		}
+		
+		if (cmd[0].equals("listLyuk")) {
+			
+		}
+		
+		if (cmd[0].equals("listMez")) {
+			
+		}
+		
+		if (cmd[0].equals("listOlaj")) {
+			
+		}
+		
+		if (cmd[0].equals("putdownMez")) {
+			
+		}
+		
+		if (cmd[0].equals("putdownOlaj")) {
+			
+		}
+	}
+}
